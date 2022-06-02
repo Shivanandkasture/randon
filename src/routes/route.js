a2864bfee6d40f19a4f87b4bf1e45df330f7d81e
ShivanandKasture <shivanandkasture1415@gmail.com>
2022-06-02 22:27:04 +0530

    adding new file lodash

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require('lodash');
 const weclome = require('../logger/logger.js');
 const helper = require('../util/helper.js');
 const formatter = require('../validator/formatter.js');
@@ -16,21 +17,29 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
+    router.get('/hello', function (req, res) {
+    const months =['January','February','March','April','May','June','July','August','September','October','November','December'];
+    const monthArray = lodash.chunk(months, 3);
+
+    const oldOddNumber = [1,3,5,7,9,11,13,15,17,19];
+    const newOddNumber = lodash.tail(oldOddNumber);
+
+    const uniqueValue = lodash.union([1,2,3,5,7,8,],[1,2,4,10],[1,2,34,11,7,8]);
+
+    const movieArray =[ ['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']]
+    const movieOject = lodash.fromPairs(movieArray);
+
+    console.log(monthArray);
+    console.log(newOddNumber);
+    console.log(uniqueValue);
+    console.log(movieOject);
+
+    res.send('Hello API.')
+
 });
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
 
 module.exports = router;
+
 // adding this comment for no reason
